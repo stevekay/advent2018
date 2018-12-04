c45c8303a5b41391ac66fce2125cda2bbfc444d3
Steve Kay <stevekay@gmail.com>
2018-12-04 22:03:53 +0000

    tidyup

--- 04/part2.pl
+++ 04/part2.pl
@@ -1,14 +1,9 @@
 #!/bin/perl
-open(A,"sort < " . $ARGV[0]. " |")||die "unable to open $ARGV[0] : $!";
-$verbose=$ARGV[1] eq '-v' ? 1 : 0;
+open(A,"sort < $ARGV[0] |")||die "unable to open $ARGV[0] : $!";
 while(<A>) {
  ($kak,$year,$month,$day,$hour,$min)=split(/\[|\-| |:|\]/);
- if($_ =~ /Guard #(\d+) begins shift/) {
-  $guard=$1;
- }
- if($_ =~ /falls asleep/) {
-  $asleep=$min;
- }
+ if($_ =~ /Guard #(\d+) begins shift/) { $guard=$1 }
+ if($_ =~ /falls asleep/) { $asleep=$min }
  if($_ =~ /wakes up/) {
   $sleep{$guard}+=$min-$asleep-1;
   for $x ( $asleep .. $min - 1 ) {
@@ -26,5 +21,3 @@ foreach $x (sort keys %b) {
 }
 
 print "Answer is $g * $c = ",$g*$c,"\n";
-exit;
-
