d09bdf8a7ff4c4ce54d75a02074c0be5a2eaa70b
Steve Kay <stevekay@gmail.com>
2018-12-18 19:26:21 +0000

    faster

---
+++ 10/part-1-and-2-fast.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl  -w
+use strict;
+my($m,$x,$y,$seconds,$minx,$miny,$maxx,$maxy,$xspeed,$yspeed);
+my @positions;
+
+open(A,$ARGV[0])||die;
+while(<A>) {
+	next unless /^position=< *(-*[0-9]+), *(-*[0-9]+)> velocity=< *(-*[0-9]+), *(-*[0-9]+)>/;
+	push @positions,[ $1, $2, $3, $4 ];
+}
+close A;
+
+do { 
+ 	$m=1;
+	if($maxy-$miny > 500) { $m = 250 }
+	$seconds+=$m;
+	$maxy=-5000000;
+	$miny=5000000;
+	$maxx=-5000000;
+	$minx=5000000;
+	for (0 .. $#positions) {
+		($x,$y,$xspeed,$yspeed)=@{$positions[$_]};
+		$x+=$xspeed*$m;
+		$y+=$yspeed*$m;
+ 		$positions[$_]=[ $x, $y, $xspeed, $yspeed ];
+		if($y>$maxy) { $maxy=$y}
+		if($y<$miny) { $miny=$y}
+		if($x<$minx) { $minx=$x}
+		if($x>$maxx) { $maxx=$x}
+	}
+} until ($maxy-$miny < 10);
+
+my @v;
+for($x=$minx;$x<=$maxx;$x++) {
+	for($y=$miny;$y<=$maxy;$y++) {
+		$v[$x+abs($minx)][$y+abs($miny)]='.';
+	}
+}
+for(@positions) {
+	($x,$y,$xspeed,$yspeed)=@{$_};
+	$v[$x+abs($minx)][$y+abs($miny)]='#';
+}
+for($y=$miny;$y<=$maxy;$y++) {
+	for($x=$minx;$x<=$maxx;$x++) {
+		print $v[$x+abs($minx)][$y+abs($miny)];	
+	}
+	print "\n";
+}
+print "seconds=$seconds\n";
