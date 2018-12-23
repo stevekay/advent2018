49b8aa6041c35c16f3c7f32c89d35a7e1dfccf77
Steve Kay <stevekay@gmail.com>
2018-12-23 22:07:52 +0000

    day 11 part 2

---
+++ 11/README.md
@@ -0,0 +1,29 @@
+Part 1
+
+:white_check_mark:
+
+    [steve@localhost 11]$ ./part1.pl 3031
+    Tue Dec 18 21:03:53 2018 x=0 of 300
+    Tue Dec 18 21:03:53 2018 x=25 of 300
+    Tue Dec 18 21:03:54 2018 x=50 of 300
+    Tue Dec 18 21:03:54 2018 x=75 of 300
+    Tue Dec 18 21:03:54 2018 x=100 of 300
+    Tue Dec 18 21:03:55 2018 x=125 of 300
+    Tue Dec 18 21:03:55 2018 x=150 of 300
+    Tue Dec 18 21:03:55 2018 x=175 of 300
+    Tue Dec 18 21:03:56 2018 x=200 of 300
+    Tue Dec 18 21:03:56 2018 x=225 of 300
+    Tue Dec 18 21:03:56 2018 x=250 of 300
+    Tue Dec 18 21:03:57 2018 x=275 of 300
+    serial 3031, (21,76) with 30
+    [steve@localhost 11]$ 
+
+Part 2
+
+:white_check_mark:
+
+    [steve@localhost 11]$ ./part1.p2 3031
+    highest was 160 with x 234 y 108 size 16
+    [steve@localhost 11]$ 
+    
+
--- 11/part2.pl
+++ 11/part2.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl
 $s=$ARGV[0];
 
+sub max ($$) { $_[$_[0] < $_[1]] }
+
 sub mycalc {
 	my($x,$y,$serial)=@_;
 
@@ -14,27 +16,53 @@ sub mycalc {
  	return $answer;
 }
 
-for($x=0;$x<300-3;$x++) {
-	if($x%25==0) { print scalar localtime," x=$x of 300\n" }
-	for($y=0;$y<300-3;$y++) {
-		$tot=0;
-		for($xd=0;$xd<3;$xd++) {
-			for($yd=0;$yd<3;$yd++) {
-				$tot+=mycalc($x+$xd,$y+$yd,$s);
-			}
-		}
-		$gridtot[$x][$y]=$tot;
+$max=300;
+#$max=48;
+#$max=4;
+for($x=1;$x<=$max;$x++) {
+	$gridval[$x]=[ (0) x $max+1 ];
+	for($y=1;$y<=$max;$y++) {
+		$gridval[$x][$y]=mycalc($x,$y,$s);
 	}
 }
+
+# init
+for($x=1;$x<=$max;$x++) {
+	for($y=1;$y<=$max;$y++) {
+		$xtot[$x][$y]=[ (0) x $max-$y ];
+	}
+}
+
+for($y=1;$y<=$max;$y++) {
+	for($x=1;$x<=$max;$x++) {
+		$v=0;
+		for($xd=$x;$xd<=$max;$xd++) {
+			$xtot[$x][$y][$xd]+=$gridval[$xd][$y]+$v;
+			$v=$xtot[$x][$y][$xd];
+			
+		}
+		
+	}
+}
+
+# search for highest grid
 $highest=-1;
-for($x=0;$x<300-3;$x++) {
-	for($y=0;$y<300-3;$y++) {
-		if($gridtot[$x][$y] > $highest) {
-			$highest=$gridtot[$x][$y];
-			$highx=$x;
-			$highy=$y;
+for($x=1;$x<=$max;$x++) {
+	for($y=1;$y<=$max;$y++) {
+		for($size=1;$size<=$max-max($x,$y)+1;$size++) {
+			$tot=0;
+			for $a ( 0 .. $size-1 ) {
+				$tot+=$xtot[$x][$y+$a][$x+$size-1];
+			}
+			if($tot > $highest) {
+				$highest=$tot;
+				$highestx=$x;
+				$highesty=$y;
+				$highestsize=$size;
+			}
+
 		}
 	}
 }
 
-print "serial $s, ($highx,$highy) with $highest\n";
+print "highest was $highest with x $highestx y $highesty size $highestsize\n";
